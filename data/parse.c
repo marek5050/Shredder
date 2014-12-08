2fb45420137c69b2dd3017831c47f42c96505302
Lakshmi Prakash <lprakash@utexas.edu>
2014-12-08 00:06:42 -0600

    Final

--- data/parse.c
+++ data/parse.c
@@ -1,7 +1,9 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include "stdio.h"
+#include "stdlib.h"
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
+#include <math.h> 
 
 #define MAX_QB 38
 #define MAX_RB 117
@@ -12,6 +14,8 @@
 #define MAX_ROSTERS 8589220
 #define MAX_PLAYS 1000
 #define SALARY_CAP 60000
+#define ACCEPT 100
+
 
 static int idx1h = 0;
 static int idx2h = 0;
@@ -28,6 +32,8 @@ struct player {
   double std_dev;
 } qb[38], rb[117], wr[150], te[82], k[33], dst[32];
 
+double _qb[38],_rb[117], _wr[150],_te[82], _k[33], _dst[32];
+
 struct roster {
   int qb;
   int rb1;
@@ -42,19 +48,36 @@ struct roster {
   int accept_ratio;
 } rosters1h[MAX_ROSTERS], rosters2h[MAX_ROSTERS];
 
+
+void updateRoster(struct roster *rost){
+
+ double points = _qb[rost->qb] + _rb[rost->rb1] + _rb[rost->rb2] + _wr[rost->wr1] + _wr[rost->wr2] + _wr[rost->wr3] + _te[rost->te];
+
+
+  if(points > ACCEPT) {
+      rost->accept_ratio++; 
+  }
+}
+
 struct plays {
   char role1[3];
   int player1;
-  int pts1;
+  double pts1;
   char role2[3];
   int player2;
-  int pts2;
+  double pts2;
 };
 
 
 void copyMe(char *from, char *to){
   while(*to++=*from++);
 }
+
+
+
+int mean = 100;
+int var = 10;
+
 
 void populateData (FILE *fp, struct player * playersarr, int l) {
   char curplayer[300];
@@ -63,22 +86,18 @@ void populateData (FILE *fp, struct player * playersarr, int l) {
   fgets(curplayer, 300, fp);
 
   while (fgets(curplayer, 300, fp) != EOF){
-    //printf ("%s\n", curplayer);    
     char* tok;
     tok = strtok (curplayer, ",");
-    //printf ("First: %s\n", tok);
     playersarr[i].rank = atoi(tok);
 
     copyMe(strtok(NULL, ","), playersarr[i].name);
     
-    //printf ("name: %s\n", playersarr[i].name);
     copyMe(strtok(NULL, ","), playersarr[i].team);
     copyMe(strtok(NULL, ","), playersarr[i].matchup);
     playersarr[i].best_rank = atoi(strtok(NULL, ","));
     playersarr[i].worst_rank = atoi(strtok(NULL, ","));
     tok = strtok(NULL, ",");
     sscanf(tok,"%lf", &playersarr[i].avg_rank);
-    //tok = strtok(NULL, ",");
     sscanf(curplayer, "%lf", &playersarr[i].std_dev);
     i++;
         
@@ -87,28 +106,37 @@ void populateData (FILE *fp, struct player * playersarr, int l) {
     }
     
   }
-  //printf ("Done reading!\n");
 }
 
-void readPlays (FILE *fp, struct plays * playsarr, int l){
+int readPlays (FILE *fp, struct plays * playsarr, int l){
   char curplay[300];
   int i = 0;
-  //char role[4];
+  char* tok;
   while (fgets(curplay, 300, fp) != EOF) {
-    char* tok;
-    tok = strtok(curplay, ",");
-    copyMe(tok, playsarr[i].role1);
-    playsarr[i].player1 = atoi(strtok(NULL, ","));
-    playsarr[i].pts1 = atoi(strtok(NULL, ","));
-    copyMe(strtok(NULL, ","), playsarr[i].role1);
-    playsarr[i].player2 = atoi(strtok(NULL, ","));
-    playsarr[i].pts2 = atoi(strtok(NULL, ","));
-    i++;
+    if(strlen(curplay) < 3) break;
+    curplay[strlen(curplay)-1] = 0;
 
-    if (i == l) {
-      break;
+    tok = strtok(curplay, ",");
+   if(tok != NULL){
+    copyMe(tok, playsarr[i].role1);
+    
+    playsarr[i].player1 = atoi(strtok(NULL, ","));
+ 
+    sscanf(strtok(NULL, ","), "%lf", &playsarr[i].pts1);
     }
+    tok = strtok(NULL, ",");
+    if(tok != NULL){
+
+    copyMe(tok, playsarr[i].role2);
+
+    playsarr[i].player2 = atoi(strtok(NULL, ","));
+    
+    sscanf(strtok(NULL, ","), "%lf", &playsarr[i].pts2);
+    }
+    
+    i++;
   }
+return i;
 }
 
 void getsalaries(){
@@ -155,24 +183,21 @@ void getsalaries(){
    
     for(i = 0; i < max; i++ ){
   	if(strcmp(name,ptr[i].name)==0){	
-	 // 	printf("%s %d \n",ptr[i].name, salary);	
-		ptr[i].salary = salary;
+	ptr[i].salary = salary;
     }
    }
   }
 }
 
 void finalizeDST (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i, int tei, int ki){
-  //printf ("In function finalizeDST");
   int dsti;
   int amount = qb[qbi].salary + rb[rb1i].salary + rb[rb2i].salary + wr[wr1i].salary + wr[wr2i].salary + wr[wr3i].salary + te[tei].salary + k[ki].salary;
  int sum;
  struct roster * rosters;
  int idx;
- for (dsti = 0; dsti < 15; dsti++) {
+ for (dsti = 0; dsti < 5; dsti++) {
     sum = amount + dst[dsti].salary;
     if (dst[dsti].salary != 0 && sum <= SALARY_CAP && sum >= 59000){
-      //printf("qbi: %d rbi1: %d rb2i: %d wr1i: %d wr2i %d wr3i %d  tei %d ki %d \n",qbi,rb1i,rb2i,wr1i,wr2i,wr3i,tei,ki);
       if (idx1h < MAX_ROSTERS){
 	rosters = rosters1h;
 	idx = idx1h;
@@ -181,7 +206,7 @@ void finalizeDST (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i, int
       else {
         rosters = rosters2h;
 	idx = idx2h;
-	idx1h++;
+	idx2h++;
       } 
       rosters[idx].qb = qbi;
       rosters[idx].rb1 = rb1i;
@@ -193,17 +218,13 @@ void finalizeDST (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i, int
       rosters[idx].k = ki;
       rosters[idx].dst = dsti;
       rosters[idx].total_amount = sum;
-      
-      
-      //printf ("index: %d\n", idx);
     }
   }
 }
 
 void finalizeK (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i, int tei){
-  //printf ("In function finalizeK");
   int ki;
-  for (ki = 0; ki < MAX_K; ki++) {
+  for (ki = 0; ki < 5; ki++) {
     if (k[ki].salary != 0){
       finalizeDST (qbi, rb1i, rb2i, wr1i, wr2i, wr3i, tei, ki);
     }
@@ -211,9 +232,8 @@ void finalizeK (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i, int t
 }
 
 void finalizeTE (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i){
-  //printf ("In function finalizeTE");
   int tei;
-  for (tei = 0; tei < MAX_TE; tei++) {
+  for (tei = 0; tei < 5; tei++) {
     if (te[tei].salary != 0){
       finalizeK (qbi, rb1i, rb2i, wr1i, wr2i, wr3i, tei);
     }
@@ -221,9 +241,8 @@ void finalizeTE (int qbi, int rb1i, int rb2i, int wr1i, int wr2i, int wr3i){
 }
 
 void finalizeWR3 (int qbi, int rb1i, int rb2i, int wr1i, int wr2i) {
-  //printf ("In function finalizeWR3");
   int wr3i;
-  for (wr3i = wr2i + 1; wr3i < 50; wr3i++) {
+  for (wr3i = wr2i + 1; wr3i < 20; wr3i++) {
     if ( wr[wr3i].salary != 0) {
       finalizeTE (qbi, rb1i, rb2i, wr1i, wr2i, wr3i);
     }
@@ -232,7 +251,6 @@ void finalizeWR3 (int qbi, int rb1i, int rb2i, int wr1i, int wr2i) {
 
 
 void finalizeWR2 (int qbi, int rb1i, int rb2i, int wr1i) {
-  //printf ("In function finalizeWR2");
   int wr2i;
   for (wr2i = wr1i + 1; wr2i < 20; wr2i++) {
     if (wr[wr2i].salary != 0) {
@@ -243,7 +261,6 @@ void finalizeWR2 (int qbi, int rb1i, int rb2i, int wr1i) {
 
 
 void finalizeWR1 (int qbi, int rb1i, int rb2i){
-  //printf ("In function finalizeWR1");
   int wr1i;
   for (wr1i = 0; wr1i < 20; wr1i++) {
     if (wr[wr1i].salary != 0){
@@ -254,7 +271,6 @@ void finalizeWR1 (int qbi, int rb1i, int rb2i){
 
 
 void finalizeRB2 (int qbi, int rb1i) {
-  //printf ("In function finalizeRB2");
   int rb2i;
   for (rb2i = rb1i + 1; rb2i < 20; rb2i++) {
     if (rb2i != rb1i && rb[rb2i].salary != 0) {
@@ -264,7 +280,6 @@ void finalizeRB2 (int qbi, int rb1i) {
 }
 
 void finalizeRB1 (int qbi) {
-  //printf ("In function finalizeRB1");
   int rb1i;
   for (rb1i = 0; rb1i < 10; rb1i++) {
     if (rb[rb1i].salary != 0){
@@ -275,7 +290,6 @@ void finalizeRB1 (int qbi) {
 
 
 void makeRosters(){
-  //printf ("In function makeRosters");
   int qbi;
   for (qbi = 0; qbi < 15; qbi++) {
     if (qb[qbi].salary != 0){
@@ -284,9 +298,82 @@ void makeRosters(){
   }
 }
 
-struct plays *teams[32];
+int nextNormal(int mean, int var){
+
+ float Y=0;
+ float U=0;
+  int i = 0;
+ float result=-1;
+while(result < 0 || result >130){ 
+                                                                        
+    Y = ((rand()%10000)/1000.0) -5.0;                                   
+    U = ((rand()%10000)/10000.0);                                       
+                                                                        
+    result = mean + Y*var;                                       
+     
+}                                
+ return (int) result;                                                               
+}
+
+int nextUniform(int max){
+   
+int r = rand()%max;
+
+return r; 
+}
+
+struct plays teams[32][1010];
+int playcount[32];
+ 
+
+void updatePlayer(struct plays *curPlay){
+
+   if(strcmp(curPlay->role1,"QB")==0){
+    	_qb[curPlay->player1] += curPlay->pts1;
+    } else if(strcmp(curPlay->role1,"RB")==0){
+    	_rb[curPlay->player1] += curPlay->pts1;
+    }else if(strcmp(curPlay->role1,"WR")==0){
+    	_wr[curPlay->player1] += curPlay->pts1;
+ 
+    }else if(strcmp(curPlay->role1, "TE") == 0){
+    	_te[curPlay->player1] += curPlay->pts1;
+
+    }else if(strcmp(curPlay->role1, "K") == 0){
+    	_k[curPlay->player1] += curPlay->pts1;
+    
+    }else if(strcmp(curPlay->role1, "DST") == 0){
+     	_dst[curPlay->player1]  = 0;
+    } 
+
+    if(strlen(curPlay->role2) > 1){
+
+    if(strcmp(curPlay->role2,"QB")==0){
+    	_qb[curPlay->player2] += curPlay->pts2;
+    } else if(strcmp(curPlay->role2,"RB")==0){
+    	_rb[curPlay->player2] += curPlay->pts2;
+    }else if(strcmp(curPlay->role2,"WR")==0){
+    	_wr[curPlay->player2] += curPlay->pts2;
+ 
+    }else if(strcmp(curPlay->role2, "TE") == 0){
+    	_te[curPlay->player2] += curPlay->pts2;
+
+    }else if(strcmp(curPlay->role2, "K") == 0){
+    	_k[curPlay->player2] += curPlay->pts2;
+    
+    }else if(strcmp(curPlay->role2, "DST") == 0){
+    	_dst[curPlay->player2] = 0;
+    } 
+    }
+}
+void eraseArray( double *arr,int size){
+  int  i = 0;
+  while( i++ < size) *(arr++) = 0;
+}
 
 int main (){
+  
+ srand(time(NULL));
+
   FILE *fpqb;
   FILE *fprb;
   FILE *fpwr;
@@ -305,67 +392,123 @@ int main (){
   populateData(fpte, te, 82);
   populateData(fpk, k, 33);
   populateData(fpdst, dst, 32);
-  // printf ("Done populating!\n");
   
   int i, j;
-  /*
-  for (j = 0; j < 33; j++){
-    printf ("%s\n", k[j].team);
-  }
-  */
   getsalaries();
-  /*
-  for (j = 0; j < 33; j++){
-    printf ("%s %d \n",k[j].name,  k[j].salary);
-  }
-  */
- // makeRosters();
+  
+  makeRosters();
  
-  int  z = 0;
-  for(;z < 32 ; z++){
-    struct plays teamPlays[100];
-    teams[z] = &teamPlays;
-  }
+int  z = 0;
 
 char *prefix = "playcsv/Fantasy_";
 char str[80];
-//strcat (str, foo);
 
+for(i=0;i < 32; i++){
+}
 
   FILE *teamplays;
   for (z = 0; z < 32; z++) {
 	strcpy(str,  prefix);
 	strcat (str, dst[z].name);
 	strcat( str, ".csv");
-	
-	printf("%s \n" , str);
        
 	if(access(str, F_OK) != -1){
-	  printf("Yes \n");
  	  teamplays = fopen(str,"r" );
-	  readPlays (teamplays,teams[z], 20);
-	
+	  if(teamplays){
+	       playcount[z] = readPlays (teamplays,teams[z], 1002);
+	   }else{
+		printf("does not exist");
+	    }
+
     	  fclose(teamplays);
 	}else{
 	  printf("No \n");
-
 	}
+  } 
+ 
+  printf(" DONE \n" );
+  int pickPlay;
+  int randomPlay;
+  int ret=0;
+  int teamnumber = 0;
+ 
+  struct roster rost; 
+  rost.qb = 10;
+  rost.rb1 = 12;
+  rost.rb2 = 3;
+  rost.wr1=1;
+  rost.wr2=2;
+  rost.wr3=3;
+  rost.te=3;
+  rost.k=3;
+  rost.dst=1;
+  rost.accept_ratio = 0 ;
+  struct plays *curPlay;
+
+
+int totalLoop;
+
+for(totalLoop=0; totalLoop < 1000; totalLoop++){
+
+ for(teamnumber=0; teamnumber < 32; teamnumber++){
+   pickPlay = nextNormal(mean, var);
+ 
+   for (ret=0;ret < pickPlay; ret++){
+     randomPlay = nextUniform(playcount[teamnumber]);
+     curPlay = &teams[teamnumber][randomPlay];
+    updatePlayer(curPlay);
+   } 
   }
-/*
-  for (i = 0; i < 5; i++) {
-    j = i * 100000;
-    printf ("QB: %s Salary: %d\n", qb[rosters1h[j].qb].name, qb[rosters1h[j].qb].salary);
-    printf ("RB: %s Salary: %d\n", rb[rosters1h[j].rb1].name, rb[rosters1h[j].rb1].salary);
-    printf ("RB: %s Salary: %d\n", rb[rosters1h[j].rb2].name, rb[rosters1h[j].rb2].salary);
-    printf ("WR: %s Salary: %d\n", wr[rosters1h[j].wr1].name, wr[rosters1h[j].wr1].salary);
-    printf ("WR: %s Salary: %d\n", wr[rosters1h[j].wr2].name, wr[rosters1h[j].wr2].salary);
-    printf ("WR: %s Salary: %d\n", wr[rosters1h[j].wr3].name, wr[rosters1h[j].wr3].salary);
-    printf ("TE: %s Salary: %d\n", te[rosters1h[j].te].name, te[rosters1h[j].te].salary);
-    printf ("K: %s Salary: %d\n", k[rosters1h[j].k].name, k[rosters1h[j].k].salary);
-    printf ("DST: %s Salary: %d\n", dst[rosters1h[j].dst].name, dst[rosters1h[j].dst].salary);
-    printf ("Total Salary: %d\n\n", rosters1h[j].total_amount);
+  
+  int xx,yy;
+  
+  for(xx=0; xx < idx1h; xx++){
+    updateRoster(&rosters1h[xx]);
   }
- */ 
- // printf ("\nNumber of rosters!!!!! %d\n", idx1h + idx2h);
+ 
+  for(yy=0; yy < idx2h; yy++){
+    updateRoster(&rosters2h[yy]);
+  }  
+
+
+  updateRoster(&rost); 
+eraseArray(_qb,MAX_QB);
+eraseArray(_rb,MAX_RB);
+eraseArray(_wr,MAX_WR);
+eraseArray(_te,MAX_TE);
+eraseArray(_k,MAX_K);
+eraseArray(_dst,MAX_DST);
+}
+
+//Filter the ones with accepting ratio more than 75% and minize on analyst rankings
+ int minrank = 99999;
+ int temp=0;
+ struct roster *minrost;
+ struct roster *ro;
+ int xx;
+ for(xx=0; xx < idx1h; xx++){
+
+  if(rosters1h[xx].accept_ratio > 0.75){
+     ro = &rosters1h[xx];
+     temp = ro->qb+ ro->rb1+ ro->rb2 + ro->wr1 + ro->wr2 + ro->wr3 + ro->te + ro->dst + ro->k;
+     if(temp < minrank){
+       minrost = ro;
+       minrank=temp;
+     }
+   }
+ }
+
+//Print our final choice
+    printf ("QB: %s Salary: %d\n", qb[minrost->qb].name, qb[minrost->qb].salary);
+    printf ("RB: %s Salary: %d\n", rb[minrost->rb1].name, rb[minrost->rb1].salary);
+    printf ("RB: %s Salary: %d\n", rb[minrost->rb2].name, rb[minrost->rb2].salary);
+    printf ("WR: %s Salary: %d\n", wr[minrost->wr1].name, wr[minrost->wr1].salary);
+    printf ("WR: %s Salary: %d\n", wr[minrost->wr2].name, wr[minrost->wr2].salary);
+    printf ("WR: %s Salary: %d\n", wr[minrost->wr3].name, wr[minrost->wr3].salary);
+    printf ("TE: %s Salary: %d\n", te[minrost->te].name, te[minrost->te].salary);
+    printf ("K: %s Salary: %d\n", k[minrost->k].name, k[minrost->k].salary);
+    printf ("DST: %s Salary: %d\n", dst[minrost->dst].name, dst[minrost->dst].salary);
+    printf ("Total Salary: %d\n\n", minrost->total_amount);
+
 return 0;
 }
